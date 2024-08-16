37927d3b6cc21527d31d481edf0666a300498468
KernelSensei <prateeksaini9871@gmail.com>
2024-08-16 09:00:57 +0530

    this is new 1

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main()
     {
         cout<<"Hello Word"<<endl; //this was done
+        cout<<"just checking"<<endl;
         cout<<"And git setup is complete"<<endl;//done 
     }   
